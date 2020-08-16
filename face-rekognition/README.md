f2703e621128b46b954054d076dcbb6907bdeaaf
Diana Patron <patrondiana13@hotmail.com>
2020-08-16 15:35:00 -0500

    Add backend for face rekognition

--- face-rekognition/README.md
+++ face-rekognition/README.md
@@ -23,8 +23,9 @@ In **User data** input the following :
     apt update
     apt install -y apache2 
     systemctl start apache2.service
-    git clone git@github.com:dianapatrong/aws-tutorials.git
-    
+    git clone https://github.com/dianapatrong/aws-tutorials.git
+    mv -r aws-tutorials/face-rekognition/front-end/* /var/www/html/
+
      
 > NOTE: EC2 User Data is automatically run with the **sudo** command.
 
@@ -33,7 +34,7 @@ In **User data** input the following :
 * Choose a **Name tag**: EC2 for Apache Web Server -> Next: Configure Security Groups
 
 #### Security Groups:
-* **Security Group Name**: apache-web-server-sg
+* **Security Group Name**: front-end-sg-face-rekognition
 * Rules
 
 | Type      | Protocol | Port Range | Source    |
@@ -42,11 +43,75 @@ In **User data** input the following :
 | HTTP      | TCP      | 80         | 0.0.0.0/0 |
 
 
-#### Step-1.2: Copy the front end files to the right place
+#### Step-1.2: Verify connection 
+
+Go to `http://<your-ec2-instance-public-ip>`
+
+On chrome, you may need to allow the site to get access to your webcam by going to the following option
+in your browser (`chrome://flags/#unsafely-treat-insecure-origin-as-secure`)
+
+![chrome flag](images/chrome_flag.png)
+
+> NOTE: To avoid this step you need to create a certificate for the site 
+
 
 
 
 ### Step-2: Back-end
+#### Step-2.1: Launch EC2 w/Ubuntu and Conda on it
+**AWS Console** -> **Services** -> **EC2** -> **Launch instance**
+
+* Select Ubuntu Server 18.04 LTS 
+* Select General purpose t2.micro (free tier elegible) -> Configure Instance Details
+* **Network**: Tutorials_VPC
+* **Subnet**: Tutorial Public Subnet
+* **Auto-assign Public IP**: Enable
+
+#### Advanced details: 
+In **User data** input the following : 
+
+    #!/bin/bash
+    apt-get update -y
+    wget https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh
+    bash Anaconda3-2020.07-Linux-x86_64.sh -b  -p /home/ubuntu/anaconda3
+    export PATH=/home/ubuntu/anaconda3/bin:$PATH
+    source ~/.bashrc
+    cd /home/ubuntu/
+    git clone https://github.com/dianapatrong/aws-tutorials.git
+    cd aws-tutorials/face-rekognition/back-end
+    conda init bash
+    conda create --name flask_venv python=3
+    conda activate flask_venv
+    conda install -y Flask
+    conda install -y flask-cors 
+    conda install -y boto3
+     
+  
+     #!/bin/bash
+     apt-get update -y
+     git clone https://github.com/dianapatrong/aws-tutorials.git
+     apt install python3
+     apt-get install -y python3-venv
+     python3 -m venv flask_venv
+     source flask_venv/bin/activate
+     pip install -r requirements.txt
+     
+     
+     
+> NOTE: EC2 User Data is automatically run with the **sudo** command.
+
+
+* Click on **Next: Add Storage** -> **Next: Add Tags**
+* Choose a **Name tag**: EC2 for Apache Web Server -> Next: Configure Security Groups
+
+#### Security Groups:
+* **Security Group Name**: back-end-sg-face-rekognition
+* Rules
+  
+| Type      | Protocol | Port Range | Source    |
+| :---:     |   :---:  | :---:      | :---      |
+| SSH       | TCP      | 22         | 0.0.0.0/0 |
+| Custom TCP| TCP      | 8888       | 0.0.0.0/0 |
 
 
 
---
+++ face-rekognition/back-end/my_rek_app.py
@@ -0,0 +1,150 @@
+from flask import Flask, request
+from flask_cors import CORS
+from binascii import a2b_base64
+import boto3
+import json
+
+app = Flask(__name__)
+CORS(app)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+
+@app.route("/get_picture1/", methods=['GET', 'POST'])
+def p1():
+    return getpicture(1)
+
+
+@app.route("/get_picture2/", methods=['GET', 'POST'])
+def p2():
+    return getpicture(2)
+
+@app.route("/", methods=['GET', 'POST'])
+def testRoute():
+    return "test"
+
+
+def getpicture(myid):
+    rdata = request.get_data()
+    image_name = 'image' + str(myid) + '.jpg'
+    save_uri_as_jpeg(rdata, image_name)
+    print("screenshot saved as %s" % image_name)
+
+    # Upload in S3 bucket
+    upload_to_S3(image_name)
+
+    # Launch Reko detect faces...
+    myjson = AWSdetect_faces(image_name)
+
+    # Extract Json infos
+    answer = get_features_from_json(myjson)
+
+    return answer
+
+
+@app.route("/compare/", methods=['GET', 'POST'])
+def comparepicture():
+    answer = AWScomparefaces()
+    print(answer)
+    return answer
+
+
+def save_uri_as_jpeg(uri, imagename):
+    imgData = str(uri)
+    imgData64 = imgData[imgData.find(',') + 1:]
+    binary_data = a2b_base64(imgData64)
+    with open(imagename, 'wb') as fd:
+        fd.write(binary_data)
+
+
+def upload_to_S3(imagename):
+    mys3 = boto3.resource('s3',region_name='us-east-1')
+    mybucket = mys3.Bucket('image-for-reko')
+    myobject = mybucket.Object(imagename)
+    myobject.delete()
+    myobject.wait_until_not_exists()
+    print("deleted")
+    myobject.upload_file(imagename)
+    myobject.wait_until_exists()
+    print("uploaded")
+
+
+def AWSdetect_faces(imagename):
+    reko = boto3.client('rekognition',region_name='us-east-1')
+    response = reko.detect_faces(
+        Image={
+            'S3Object': {
+                'Bucket': 'image-for-reko',
+                'Name': imagename,
+            }
+        },
+        Attributes=[
+            'ALL',
+        ]
+    )
+    return response
+
+
+def AWScomparefaces():
+    reko = boto3.client('rekognition',region_name='us-east-1')
+
+    response = reko.compare_faces(
+        SourceImage={
+            'S3Object': {
+                'Bucket': 'image-for-reko',
+                'Name': 'image1.jpg',
+            }
+        },
+        TargetImage={
+            'S3Object': {
+                'Bucket': 'image-for-reko',
+                'Name': 'image2.jpg',
+            }
+        },
+        SimilarityThreshold=90
+    )
+
+    FaceMatch = response['FaceMatches']
+    mystr = "<kbd> Similarity = "
+    if len(FaceMatch) > 0:
+        FirstMatch = FaceMatch[0]
+        mystr += "%.2f%%" % FirstMatch['Similarity']
+    else:
+        mystr += "No Matching face"
+    mystr += "</kbd>"
+    return mystr
+
+
+def get_features_from_json(myjson):
+    mystr = ""
+    facedetails = myjson['FaceDetails']
+    nbfaces = len(facedetails)
+    notusedattributes = ['BoundingBox', 'Landmarks', 'Pose', 'Quality', 'Confidence']
+    if nbfaces == 1:
+        face = facedetails[0]
+        mystr += '<table class="table table-sm table-striped bg-light m-2">'
+        for attribute, details in face.items():
+            if attribute not in notusedattributes:
+                mystr += '<tr>'
+                if attribute == 'AgeRange':
+                    mystr += "<td>%s</td><td>%d</td><td>%d yo</td>" % (attribute, details['Low'], details['High'])
+                elif attribute != "Emotions":
+                    mystr += "<td>%s</td><td>%s</td><td>%.2f%%</td>" % (
+                    attribute, details['Value'], details['Confidence'])
+                else:
+                    for emotion in details:
+                        if emotion['Confidence'] > 50:
+                            mystr += "<td>Emotion</td><td>%s</td><td>%.2f%%</td>" % (
+                            emotion['Type'], emotion['Confidence'])
+                mystr += "</tr>"
+        mystr += "</table>"
+    elif nbfaces > 1:
+        mystr += "%d faces found on picture...\n" % len(facedetails)
+    else:
+        mystr += "Nobody on picture...\n"
+    return mystr
+
+
+if __name__ == "__main__":
+    # run the app locally on the given port
+    app.run(host='0.0.0.0', port=5000)
